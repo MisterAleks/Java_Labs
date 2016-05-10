180a58aac8c4440c7e47bcad7b54c734d5d1f1a0
Алексей <alekseychernavskiy@gmail.com>
2016-05-11 03:14:28 +0500

    • Deleted package My (with my own methods, they were useless) • Started development Lab_6 the third task

--- src/Labs_Java/Lab_6/Animal.java
+++ src/Labs_Java/Lab_6/Animal.java
@@ -1,10 +1,23 @@
 package Labs_Java.Lab_6;
 
+import java.io.BufferedReader;
+
 /**
  * Created by Алексей on 11.05.2016.
  */
 public class Animal {
     private int age;
-    private String gender;
-    private
+    private int power;
+
+    public int getAge() {
+        return age;
+    }
+
+    public int power() {
+        return power;
+    }
+
+    public Animal() {
+
+    }
 }
