ebc27dcd784f412d2b12c672486fd2765294b11b
Алексей <alekseychernavskiy@gmail.com>
2016-05-18 03:43:04 +0500

    Added Lab_8

---
+++ src/Labs_Java/Lab_8/Cat.java
@@ -0,0 +1,20 @@
+package Labs_Java.Lab_8;
+
+/**
+ * Created by Алексей on 18.05.2016.
+ */
+public class Cat {
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Cat(String name) {
+        this.name = name;
+    }
+}
